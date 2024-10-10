f17038c8e2072f3ad240f844a8a9bdf0e5c60112
Sushil Tyagi <tyagi@berkeleymarinerobotics.com>
2024-10-09 22:12:44 -0700

    various tests edits

--- HW_01.R
+++ HW_01.R
@@ -1,16 +1,19 @@
----
-title: "Homework 1"
-format: html
----
+#---
+#title: "Homework 1"
+#format: html
+#---
 
-__Name:__ \<your name here\>
+#__Name:__ \<your name here\>
 
 
-__Question 1:__ Use a programming language of your choice to save today's date to a variable.
-Then, include the date in the text below the code block.
+#__Question 1:__ Use a programming language of your choice to save today's date to a variable.
+#Then, include the date in the text below the code block.
 
-```
+#```
 # your code here
-```
+x <- Sys.Date()
+print(x)
+#```
 
-Today's date is: `<your answer here>`.
+#Today's date is: `<2024-10-09>`.
+
